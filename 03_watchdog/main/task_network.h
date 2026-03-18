4b09464cc52d07470806e33d83381fb3aa3a84d7
grerfou <benoit.fage@icloud.com>
2026-03-18 12:24:26 +0100

    chore: keep only final project, ignore intermediate steps

--- 03_watchdog/main/task_network.h
+++
@@ -1,9 +0,0 @@
-#ifndef TASK_NETWORK8H
-#define TASK_NETWORK8H
-
-#include "freertos/FreeRTOS.h"
-
-/* Proto tache  */
-void task_network(void *pvParameters);
-
-#endif /* TASK_NETWORK_H  */
--- 03_watchdog/main/task_watchdog.h
+++
@@ -1,9 +0,0 @@
-#ifndef TASK_WATCHDOG_H
-#define TASK_WATCHDOG_H
-
-#include "freertos/FreeRTOS.h"
-
-/* Proto Tache  */
-void task_watchdog(void *pvParameters);
-
-#endif /* TASK_WATCHDOG_H  */
